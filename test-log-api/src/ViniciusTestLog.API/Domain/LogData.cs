d466c3134f4a63e592c0d7b9d18ee62b8ee34cbb
Vinicius Luna <viniciuscluna@gmail.com>
2022-12-21 22:32:19 -0300

    Removed unused Id from LogData

--- test-log-api/src/ViniciusTestLog.API/Domain/LogData.cs
+++ test-log-api/src/ViniciusTestLog.API/Domain/LogData.cs
@@ -1,13 +1,7 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
-
-namespace ViniciusTestLog.API.Domain
+﻿namespace ViniciusTestLog.API.Domain
 {
     public class LogData
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
         public DateTime Date { get; set; }
         public string? Ip { get; set; }
         public string? Category { get; set; }
